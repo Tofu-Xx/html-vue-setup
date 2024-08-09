54e01df53bbe93911ad93660afb7b09afdfdf846
Tofu <1714843907@qq.com>
2024-08-09 17:11:08 +0800

    chore:filename

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/",
   build: {
-
     lib: {
-      entry: "src/index.js",
+      entry: "src/main.js",
       formats: ["umd"],
       name: "html-vue-setup",
+      fileName: (format) => `main.${format}.js`,
     },
   }
 });
