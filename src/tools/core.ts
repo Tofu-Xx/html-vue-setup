70c9f48fc287d0ce701ff62d329b9a4a970b1afc
Tofu <1714843907@qq.com>
2024-08-26 03:30:02 +0800

    fix: getExposedName retrun wrong variable name

--- src/tools/core.ts
+++ src/tools/core.ts
@@ -3,7 +3,14 @@ export function getExposedName(scriptContent: string) {
     /(?:let|const|function)\s+\[?\{?\s*([a-zA-Z_$][\w$,\s]*)\b/g;
   return [...scriptContent.matchAll(globalVarRegex)]
     .flatMap((match) => match[1].split(",").map((v) => v.trim()))
-    .filter(Boolean);
+    .filter((el) => {
+      try {
+        eval(el);
+      } catch (e) {
+        return false;
+      }
+      return true;
+    });
 }
 /*  */
 
